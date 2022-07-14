2e0f066a8cc8b1e50cdbba916db5047db01af0d4
Donovan Duperrois <slashhable@gmail.com>
2022-07-14 10:00:41 +0200

    Ajout d'une fonction de calcul des jours restants + conditions pour vérifier si la date à comparer à déjà été passée

--- Modules/simple/log.js
+++ Modules/simple/log.js
@@ -1,62 +1,63 @@
 
 let year = new Date().getFullYear()
-let month = new Date().getMonth()+1
-let day = new Date().getDate()
 
-
-
-    function day_before_christmas(){
-        let date1 = new Date()
-        let date2 = new Date(2022,11,25)
-        return (Math.floor((date2 - date1) / (1000*3600*24)))
-        
-    }
-    function day_before_birthday(){
-        let date1 = new Date()
-        let date2 = new Date(2022,11,13)
-        return (Math.floor((date2 - date1) / (1000*3600*24)))
-    }
-    function day_before_vacation(){
-        let date1 = new Date()
-        let date4 = new Date(2022,06,21)
-        return (Math.floor((date4 - date1) / (1000*3600*24)))
-    }
-    function day_before_vacation(){
-        let date1 = new Date()
-        let date5 = new Date(2022,11,21)
-        return (Math.floor((date5 - date1) / (1000*3600*24)))
-    }
-    
-
-    // function(){
-    //     console.log(`Il reste ${birthday} avant mon anniversaire !`)
-    // }
-
-    // function(){
-    //     console.log(`Il reste ${vacation} avant Noêl !`)
-    // }
+function calcul (firstDate,secondDate ){
+    return (Math.ceil((secondDate - firstDate) / (1000*3600*24)));
+}
 
 let log ={
     noel : function day_before_christmas(){
-        let date1 = new Date()
-        let date2 = new Date(2022,11,25)
-        console.log(`il reste ${(Math.ceil((date2 - date1) / (1000*3600*24)))} jours avant noël.`) 
+        let date1 = new Date();
+        let date2 = new Date(year,11,25);
+
+        if(date1 > date2){
+            date2 = new Date(year+1,11,25);
+            console.log(`il reste ${calcul(date1,date2)} jours avant noël.`) ;
+        }
+        else{
+        console.log(`il reste ${calcul(date1,date2)} jours avant noël.`); 
+        }
         
     },
     anniversaire : function day_before_birthday(){
         let date1 = new Date()
-        let date2 = new Date(2022,11,13)
-        console.log(`Mon anniversaire sera dans ${(Math.ceil((date2 - date1) / (1000*3600*24)))} jours.`) 
+        let date2 = new Date(year,11,13)
+
+        if(date1 > date2){
+            date2 = new Date(year+1,11,13)
+            console.log(`Mon anniversaire sera dans ${calcul(date1,date2)} jours.`)  
+        }
+        else{
+            console.log(`Mon anniversaire sera dans ${calcul(date1,date2)} jours.`) 
+        
+        }
     },
     vacance : function day_before_vacation(){
         let date1 = new Date()
-        let date2 = new Date(2022,06,21)
-        console.log(`les vacances seront dans ${(Math.ceil((date2 - date1) / (1000*3600*24)))} jours.`) 
+        let date2 = new Date(year,06,21)
+
+        if(date1 > date2){
+            date2 = new Date(year+1,06,21)
+            console.log(`les vacances seront dans ${calcul(date1,date2)} jours.`)  
+        }
+        else{
+            console.log(`les vacances seront dans ${calcul(date1,date2)} jours.`) 
+        
+        }        
+        
     },
     solstice: function day_before_vacation(){
         let date1 = new Date()
-        let date2 = new Date(2022,11,21)
-        console.log(`Dans ${(Math.ceil((date2 - date1) / (1000*3600*24)))} jours, le prochain solstice aura lieu.`)
+        let date2 = new Date(year,11,21)
+
+        if(date1 > date2){
+            date2 = new Date(year+1,11,21)
+            console.log(`Dans ${calcul(date1,date2)} jours, le prochain solstice aura lieu.`) 
+        }
+        else{
+            console.log(`Dans ${calcul(date1,date2)} jours, le prochain solstice aura lieu.`)
+        
+        }    
     }
 }
 module.exports = log
